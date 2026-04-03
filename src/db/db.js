21c08561bc54463c0eadef48bac2a7f471703fb3
Raj Prasad <rajp182138@gmail.com>
2026-04-03 05:39:45 +0530

    setup of database and and express

---
+++ src/db/db.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(process.env.MONGO_URI, {
+            dbName: DB_NAME,
+          
+        });
+        console.log(`Connected to MongoDB`);
+
+    }
+    catch(err){
+        console.error("Error connecting to MongoDB:", err);
+        throw err;
+    }
+};
+
+export default connectDB;
