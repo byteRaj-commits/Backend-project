f89086c8e8d45c812b3a3dd2d969b45dd93baaff
Raj Prasad <rajp182138@gmail.com>
2026-04-04 00:19:03 +0530

    detailing the data of video model

--- Backend/src/models/video.models.js
+++ Backend/src/models/video.models.js
@@ -1,5 +1,41 @@
 import mongoose from 'mongoose';
 
-const videoSchema = new mongoose.Schema({}, {timestamps: true})
+const videoSchema = new mongoose.Schema({
+
+    videoFile:{
+        type: String,
+        required: true,
+    },
+    thumbnail: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    duration: {
+        type: Number,      // from cloudinary response
+        required: true,
+    },
+    views: {
+        type: Number,
+        default: 0,
+    },
+    isPublished: {
+        type: Boolean,
+        default: true,
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User,'
+    }
+
+}, {timestamps: true})
 
 export const Video = mongoose.model('Video', videoSchema);
