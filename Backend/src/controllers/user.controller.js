a0bae87a3bb5c834b36bb590f20cb27eb49c71ac
Raj Prasad <rajp182138@gmail.com>
2026-04-08 05:42:43 +0530

    validation and upload profile image

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,12 +1,26 @@
-import {asyncHandler} from '../utils/asyncHandler.js';
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { apiError } from "../utils/apiError.js";
+import User from "../models/user.model.js";
 
-const registerUser = asyncHandler( async (req, res) =>{
-    
-    const {name, email, username, password} = req.body;
-    if(!name || !email || !username || !password){
-        apiError(400, "please provide all the details")
-    }
+const registerUser = asyncHandler(async (req, res) => {
+  const { fullName, email, username, password } = req.body;
 
-})
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new apiError(400, "fields cannot be empty");
+  }
 
-export {registerUser}
+  const existedUser = await User.findone({
+    $or: [{ email }, { username }],
+  });
+
+  if (existedUser) {
+    throw new apiError(409, "user already exists");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+});
+
+export { registerUser };
