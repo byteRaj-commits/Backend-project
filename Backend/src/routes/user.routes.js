56c4795af09780e5b5ebd6d51e20f3df72c37765
Raj Prasad <rajp182138@gmail.com>
2026-04-10 14:39:15 +0530

    create all api endpoints of user

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,26 +1,46 @@
-import {Router} from 'express';
-import {loginUser, registerUser, logoutUser, refreshAccessToken} from '../controllers/user.controller.js';
-import {upload} from '../middlewares/multer.middleware.js'
-import { verifyJWT } from '../middlewares/auth.middleware.js';
+import { Router } from "express";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  changePassword,
+  getCurrentUser,
+  updateAvatar,
+  updateCoverImage,
+  updateUserProfile,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
-router.route('/register').post(
-    upload.fields([
-        {
-            name: 'avatar',
-            maxCount: 1
-        },
-        {
-            name: 'coverImage',
-            maxCount: 1
-        }
-    ]),
-    registerUser
-);
 
-router.route('/login').post(loginUser);
 
-router.route('/logout').post(verifyJWT, logoutUser);
-router.route('/refresh-token').post(refreshAccessToken);
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+); // register user
+router.route("/login").post(loginUser); // login user
+router.route("/logout").post(verifyJWT, logoutUser); // logout user
+router.route("/refresh-token").post(refreshAccessToken); // refresh access token
+router.route("/change-password").post(verifyJWT, changePassword); // change password
+router.route("/me").get(verifyJWT, getCurrentUser); // get current user details
+router.route("/update-profile").put(verifyJWT, updateUserProfile); // update user profile
+router
+  .route("/update-avatar")
+  .put(verifyJWT, upload.single("avatar"), updateAvatar); // update avatar
+
+router
+  .route("/update-cover-image")
+  .put(verifyJWT, upload.single("coverImage"), updateCoverImage); // update cover image
 
 export default router;
